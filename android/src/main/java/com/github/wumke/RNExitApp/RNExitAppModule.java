6c78a9f8b1e3d24384e5d01275cd9aab018beaf5
Iván Martínez Mateu <imm@ivanmartinezmateu.com>
2023-05-19 10:59:48 +0200

    Update RNExitAppModule.java

--- android/src/main/java/com/github/wumke/RNExitApp/RNExitAppModule.java
+++ android/src/main/java/com/github/wumke/RNExitApp/RNExitAppModule.java
@@ -1,30 +1,19 @@
 package com.github.wumke.RNExitApp;
 
-import android.app.AlarmManager;
-import android.app.Application;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
-import android.util.Log;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
+import android.app.Activity;
+
 
 public class RNExitAppModule extends ReactContextBaseJavaModule {
 
     ReactApplicationContext reactContext;
-    AlarmManager alarmManager;
 
     public RNExitAppModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
-        alarmManager = (AlarmManager) reactContext.getSystemService(Context.ALARM_SERVICE);
     }
 
     @Override
@@ -34,6 +23,7 @@ public class RNExitAppModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void exitApp() {
-        android.os.Process.killProcess(android.os.Process.myPid());
+        final Activity activity = getCurrentActivity();
+        activity.finishAndRemoveTask();
     }
 }
